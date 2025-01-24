898eb6856c45f198fb53c4c0c4062e89a19a9a07
hxdes08 <luca.pichler.14@gmail.com>
2025-01-24 23:59:56 +0100

    Implementation of clickable Icon to get to Subpage

--- app/src/main/java/com/example/swush/MainActivity.kt
+++ app/src/main/java/com/example/swush/MainActivity.kt
@@ -11,22 +11,19 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.SegmentedButtonDefaults
-import androidx.compose.material3.SegmentedButtonDefaults.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.vectorResource
+import android.content.Intent
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.swush.ui.theme.SwushTheme
-import androidx.compose.material3.Icon as Icon
+import androidx.compose.foundation.Image
 
 
 class MainActivity : ComponentActivity() {
@@ -44,7 +41,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Homescreen() {
-    // Hole die Bildschirmgröße
+    // Bildschirmgröße des jeweiligen Geräts
     val configuration = LocalConfiguration.current
     val screenWidth = configuration.screenWidthDp
     val screenHeight = configuration.screenHeightDp
@@ -53,37 +50,44 @@ fun Homescreen() {
         modifier = Modifier
             .fillMaxSize()
             .background(Color.White)
-    ) {
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(65.dp)
-            .background(Color(0xFF4A74BC))
-            .align(Alignment.BottomEnd)
     )
-    }
 }
 
 @Composable
-fun Iconbuttonbar() {
+fun Iconbuttonbar() { //Bar für die Icons im Homescreen
+    val context = LocalContext.current
     Box(
         modifier = Modifier
-            .padding(16.dp)
-            .fillMaxSize())
+            .fillMaxSize()
+    )
     {
-        IconButton(
-            onClick = {},
+        Box(
             modifier = Modifier
-                .align(Alignment.BottomCenter)
-                .offset(y = 10.dp)
-        ) {
-            Icon(
-                painter = painterResource(id = R.drawable.bus_icon_bar),
-                contentDescription = "Bus Icon",
-                tint = Color.Black,
-                modifier = Modifier.size(70.dp)
-            )
+                .fillMaxWidth()
+                .height(85.dp)
+                .background(Color(0xFF4A74BC)) //blaue Iconbar des Homescreens
+                .align(Alignment.BottomEnd)
+        )
+        {
+            IconButton( //Bus Icon Button für SecondActivity
+                onClick = {
+                    val intent = Intent(context, MainActivity2::class.java).apply {
+                        addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+                    }
+                    context.startActivity(intent)
+                },
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .offset(y = (-15).dp)
+            ) {
+                val icon_bus_hs = painterResource(id = R.drawable.bus_icon_bar)
+                Image(
+                    painter = icon_bus_hs,
+                    contentDescription = "Bus Icon Homescreen",
+                    modifier = Modifier.size(60.dp)
+                )
 
+            }
         }
     }
 }
